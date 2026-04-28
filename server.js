90956bf717fb257d743c2eb73766e05de840bb8f
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-04-28 17:38:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 // Middleware
 const corsOptions = {
   origin: [
+    "http://localhost:5173",
     "http://localhost:5174",
     "http://localhost:3000",
     "https://adityainstitutemanagement.com"
