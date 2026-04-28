f6f76945bc6b2e4e3f85402eeda368e794c7c211
vaishnavi manikeri <155414266+VaishnaviManikeri@users.noreply.github.com>
2026-04-28 17:33:06 +0530

    Update Blog.js

--- models/Blog.js
+++ models/Blog.js
@@ -5,6 +5,11 @@ const blogSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    slug: {
+        type: String,
+        required: true,
+        unique: true
+    },
     content: {
         type: String,
         required: true
@@ -19,7 +24,29 @@ const blogSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     },
-    tags: [String]
+    tags: [String],
+    metaTitle: String,
+    metaDescription: String,
+    readingTime: Number,
+    views: {
+        type: Number,
+        default: 0
+    }
+});
+
+// Generate slug from title before saving
+blogSchema.pre('save', function(next) {
+    if (this.isModified('title')) {
+        this.slug = this.title
+            .toLowerCase()
+            .replace(/[^a-z0-9]+/g, '-')
+            .replace(/^-|-$/g, '');
+        
+        // Calculate reading time (average 200 words per minute)
+        const wordCount = this.content.split(/\s+/).length;
+        this.readingTime = Math.ceil(wordCount / 200);
+    }
+    next();
 });
 
 module.exports = mongoose.model('Blog', blogSchema);
